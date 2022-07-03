9e62f39e3b30faa6678265cd9ccbcc8d5a5fe254
Veronika Golanova <72078802+vegolanova@users.noreply.github.com>
2022-07-03 13:32:32 +0200

    Create 1multiples_3_5.go

---
+++ Go/Project Euler/1multiples_3_5.go
@@ -0,0 +1,29 @@
+/*
+If we list all the natural numbers below 10 that are multiples
+of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+Find the sum of all the multiples of 3 or 5 below 1000.
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	const threshold = 1000
+	var multiples = []int{}
+	var result int
+
+	for i := 1; i < threshold; i++ {
+
+		if i%3 == 0 || i%5 == 0 {
+			multiples = append(multiples, i)
+		} else {
+			continue
+		}
+	}
+
+	for j := 0; j < len(multiples); j++ {
+		result += multiples[j]
+	}
+	fmt.Println(result)
+}
