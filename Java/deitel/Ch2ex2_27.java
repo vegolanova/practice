6ab74455a2e9e7cbe7362047b9570cf5f31f79da
Veronika Golanova <72078802+vegolanova@users.noreply.github.com>
2022-07-17 11:57:01 +0200

    Create Ch2ex2_27.java

---
+++ Java/deitel/Ch2ex2_27.java
@@ -0,0 +1,11 @@
+public class Ch2ex2_27 {
+    public static void main(String[] args) {
+        int counter = 0;
+
+        while (counter < 4) {
+            System.out.println("* * * * * * * *");
+            System.out.println(" * * * * * * * *");
+            counter++;
+        }
+    }
+}
