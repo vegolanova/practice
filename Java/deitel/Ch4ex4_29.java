1760eed8e916fed87b4564ae54ecfaa2f9d93de5
Veronika Golanova <72078802+vegolanova@users.noreply.github.com>
2022-08-06 19:42:00 +0200

    Create Ch4ex4_29.java

---
+++ Java/deitel/Ch4ex4_29.java
@@ -0,0 +1,35 @@
+public class Ch4ex4_29 {
+    public static void main(String[] args) {
+        for (int day = 1; day <= 12; day++) {
+            switch (day) {
+                case 1 -> System.out.println("\nOn the first day of Christmas my true love sent to me");
+                case 2 -> System.out.println("\nOn the second day of Christmas my true love sent to me");
+                case 3 -> System.out.println("\nOn the third day of Christmas my true love sent to me");
+                case 4 -> System.out.println("\nOn the fourth day of Christmas my true love sent to me");
+                case 5 -> System.out.println("\nOn the fifth day of Christmas my true love sent to me");
+                case 6 -> System.out.println("\nOn the sixth day of Christmas my true love sent to me");
+                case 7 -> System.out.println("\nOn the sevens day of Christmas my true love sent to me");
+                case 8 -> System.out.println("\nOn the eighth day of Christmas my true love sent to me");
+                case 9 -> System.out.println("\nOn the ninth day of Christmas my true love sent to me");
+                case 10 -> System.out.println("\nOn the tenth day of Christmas my true love sent to me");
+                case 11 -> System.out.println("\nOn the eleventh day of Christmas my true love sent to me");
+                case 12 -> System.out.println("\nOn the twelfth day of Christmas my true love sent to me");
+            }
+
+            switch (day) {
+                case 1 -> System.out.println("A partridge in a pear tree,");
+                case 2 -> System.out.println("Two turtle doves,");
+                case 3 -> System.out.println("Three French hens,");
+                case 4 -> System.out.println("Four calling birds,");
+                case 5 -> System.out.println("Five gold rings,");
+                case 6 -> System.out.println("Six geese a-laying,");
+                case 7 -> System.out.println("Seven swans a-swimming,");
+                case 8 -> System.out.println("Eight maids a-milking,");
+                case 9 -> System.out.println("Nine ladies dancing,");
+                case 10 -> System.out.println("Ten lords a-leaping,");
+                case 11 -> System.out.println("Eleven pipers piping,");
+                case 12 -> System.out.println("Twelve drummers drumming.");
+            }
+        }
+    }
+}
