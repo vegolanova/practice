9159b30fdba5637c1282574a032459999a05d6ce
Veronika Golanova <72078802+vegolanova@users.noreply.github.com>
2022-08-06 14:10:29 +0200

    Create Ch4ex4_24.java

---
+++ Java/deitel/Ch4ex4_24.java
@@ -0,0 +1,10 @@
+public class Ch4ex4_24 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++) {
+                System.out.printf("%d ", i);
+            }
+            System.out.println();
+        }
+    }
+}
